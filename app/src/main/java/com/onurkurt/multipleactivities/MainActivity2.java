d33644b47ea6efceee7b9bb1304ecaf9425cb3f3
DESKTOP-TAKTNKE\resat <onur.kurt@volthread.com>
2022-06-04 17:13:57 +0300

    Initial commit

---
+++ app/src/main/java/com/onurkurt/multipleactivities/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.onurkurt.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView2);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userInput");
+        textView.setText(userName);
+    }
+
+    public void changeScreen(View view){
+
+        Intent intent =new Intent (MainActivity2.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
